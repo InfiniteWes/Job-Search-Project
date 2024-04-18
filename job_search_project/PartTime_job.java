5e2a90ee250d18c03b188fa6ca37bb14e89aa0f0
Wesley Spangler <wesspangler98@gmail.com>
2024-04-18 15:46:15 -0500

    update

--- job_search_project/PartTime_job.java
+++ job_search_project/PartTime_job.java
@@ -1,5 +1,14 @@
 package job_search_project;
 
 public class PartTime_job {
-    
+    protected double hourlyRate;
+    protected int maxHours;
+
+    PartTime_job(double hourlyRate, int maxHours) {
+
+    }
+
+    public double calculateWeeklyPay() {
+        return hourlyRate * maxHours;
+    }
 }
